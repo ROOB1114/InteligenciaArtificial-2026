c682db0e1daf8b23c7d5111889c9a3b9cf9b5417
ROOB114 <roberto_arce14@outlook.es>
2026-05-26 00:26:44 -0700

    Proyecto Narrador

--- Tetris-Narrator/narracion.py
+++ Tetris-Narrator/narracion.py
@@ -66,7 +66,7 @@ class narracion:
         # Cliente para generar comentarios dinámicos en tiempo real
         print("Configurando cliente oficial de Hugging Face SDK para el comentarista...")
         
-        hf_token = os.environ.get("HF_TOKEN", "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")
+        hf_token = os.environ.get("HF_TOKEN", "INSERTAR TOKEN")
         self.cliente_hf = InferenceClient(token=hf_token)
 
         # ====== INICIALIZAR ESTADO Y MEMORIA ======
